cbaf60c447b72a6356e9a9fd3d56822f3715c34b
Rudy <rudyetzel@gmail.com>
2022-03-15 23:39:42 -0400

    second commit, formatted

--- src/HangmanTester.java
+++ src/HangmanTester.java
@@ -6,62 +6,51 @@ import java.util.List;
 import java.util.Arrays;
 
 public class HangmanTester {
-	
+
 	public int lives = 5;
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		int guesses = 0;
-		
+
 		System.out.println("Enter the word you would like the player to guess");
 		String word = in.next();
-		
+
 		String string[] = word.split("");
 		List<String> splitString = new LinkedList<>(Arrays.asList(string));
-	
-		
-		
-		
+
 		System.out.println("There are " + splitString.size() + " letters in the word.");
-		System.out.println("Would you like to guess a letter or guess the full word? A wrong guess will cost you one of your 5 lives.");
+		System.out.println(
+				"Would you like to guess a letter or guess the full word? A wrong guess will cost you one of your 5 lives.");
 		System.out.println("Say '1' to guess a letter, or '2' to guess a word");
 		int choice = in.nextInt();
-		
-		switch (choice)
-		{
+
+		switch (choice) {
 		// Letter guess
-		case 1: System.out.println("What letter would you like to guess?");
-				String guess = in.next();
-				
-				for(int i = 0; i < splitString.size(); i++)
-				{
-					if(guess.equals(splitString.get(i)))
-					{
-						splitString.remove(i);
-						System.out.println("There are " + splitString.size() + " letters remaining");
-					}
-					
+		case 1:
+			System.out.println("What letter would you like to guess?");
+			String guess = in.next();
+
+			for (int i = 0; i < splitString.size(); i++) {
+				if (guess.equals(splitString.get(i))) {
+					splitString.remove(i);
+					System.out.println("There are " + splitString.size() + " letters remaining");
 				}
-				
-					
-				break;
+
+			}
+
+			break;
 		}
-		
+
 	}
-	
-	public void letterGuess(String guess, ArrayList<String> splitString)
-	{
-		for(int i = 0; i < splitString.size(); i++)
-		{
-			if(guess == splitString.get(i))
-					{
-						splitString.remove(i);
-						System.out.println("There are " + splitString.size() + " letters remaining");
-					}
-			else
-			{
+
+	public void letterGuess(String guess, ArrayList<String> splitString) {
+		for (int i = 0; i < splitString.size(); i++) {
+			if (guess == splitString.get(i)) {
+				splitString.remove(i);
+				System.out.println("There are " + splitString.size() + " letters remaining");
+			} else {
 				lives--;
 				System.out.println("Sorry! That letter is not here.");
 				System.out.println("You have " + lives + " remaining.");
@@ -70,5 +59,3 @@ public class HangmanTester {
 	}
 
 }
-
-
